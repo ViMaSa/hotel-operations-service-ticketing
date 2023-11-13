4ebf6c7c7fa2bd1e4e5646b8f6b6cb437f435560
Victor Sarmiento <sarmiv07@gmail.com>
2023-11-13 02:55:26 -0800

    Front-end validation & communication with backend

--- frontend/src/views/DashboardPage.vue
+++ frontend/src/views/DashboardPage.vue
@@ -1,17 +1,13 @@
 <template>
   <div>
+    DashboardPage
   </div>
 </template>
 
 <script>
-// import HelloWorld from "./components/HelloWorld.vue"
-
-// export default {
-//   name: "App",
-//   components: {
-//     HelloWorld
-//   }
-// }
+export default {
+  name: "DashboardPage",
+}
 </script>
 
 <style>
--- frontend/src/views/RegisterPage.vue
+++ frontend/src/views/RegisterPage.vue
@@ -5,25 +5,37 @@
         <font-awesome-icon icon="bell-concierge" class="fa-3x me-2" style="color: #2aad29;"/>
         <h1 class="ms-2">H.O.S.T.</h1>
       </div>
-      <form class="m-5">
-        <div class="mb-3">
+      <form class="m-5" @submit.prevent="handleSubmit" novalidate>
+        <div class="mb-3" :class="{ 'has-validation': submitted && !form.email }">
           <label for="email" class="form-label">Email address</label>
-          <input type="email" class="form-control" id="email">
+          <input v-model="form.email" type="email" class="form-control" id="email" placeholder="example@email.com" required>
+          <div v-if="submitted && !form.email" class="invalid-feedback">
+            Email is required.
+          </div>
         </div>
         <div class="mb-3 row">
-          <div class="col me-5">
+          <div class="col me-5" :class="{ 'has-validation': submitted && !form.first_name}">
             <label for="first_name" class="form-label">First name</label>
-            <input type="text" class="form-control" id="first_name">
+            <input v-model="form.first_name" type="text" class="form-control" id="first_name" placeholder="First name" required>
+            <div v-if="submitted && !form.first_name" class="invalid-feedback">
+              First name is required.
+            </div>
           </div>
-          <div class="col">
+          <div class="col" :class="{ 'has-validation': submitted && !form.last_name}">
             <label for="last_name" class="form-label">Last name</label>
-            <input type="text" class="form-control" id="last_name">
+            <input v-model="form.last_name" type="text" class="form-control" id="last_name" required>
+            <div v-if="submitted && !form.last_name" class="invalid-feedback">
+              Last name is required.
+            </div>
           </div>
         </div>
-        <div class="mb-3">
+        <div class="mb-3" :class="{ 'has-validation': submitted && !form.password}">
           <label for="password" class="form-label">Password</label>
-          <input type="password" class="form-control" id="password" aria-describedby="passwordHelp">
+          <input v-model="form.password" type="password" class="form-control" id="password" aria-describedby="passwordHelp" placeholder="Password" required>
           <div id="passwordHelp" class="form-text"> We'll never share your password with anyone else.</div>
+          <div v-if="submitted && !form.password" class="invalid-feedback">
+            Password is required.
+          </div>
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
       </form>
@@ -34,8 +46,42 @@
 <script>
 export default {
   name: 'RegisterPage',
+  data() {
+    return {
+      form: {
+        email: '',
+        first_name: '',
+        last_name: '',
+        password: '',
+      },
+      submitted: false,
+    }
+  },
+  methods: {
+    async handleSubmit () {
+      this.submitted = true;
+      if(this.form.email && this.form.first_name && this.form.last_name && this.form.password) {
+        try {
+          console.log("hello")
+          // Send a POST request to your API endpoint
+          const response = await this.$http.post('/api/user/register', this.form);
+          // Handle success
+          console.log(response.data);
+          // Redirect or update UI as needed
+          // this.$router.push('/dashboard');
+      } catch (error) {
+        // Handle error
+        console.error(error.response ? error.response.data : error.message);
+        // Possibly show an error message to the user
+      }
+      }
+    }
+  }
 }
 </script>
 
-<style>
+<style scoped>
+.invalid-feedback {
+  display: block;
+}
 </style>
