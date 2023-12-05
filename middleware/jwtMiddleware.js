deb4e4658be2a47ff2180147a083ecc10936397e
Victor Sarmiento <sarmiv07@gmail.com>
2023-12-04 21:59:21 -0800

    nav-bar reacts to user being signed in/out

--- middleware/jwtMiddleware.js
+++ middleware/jwtMiddleware.js
@@ -3,7 +3,7 @@ const SECRET_KEY = process.env.JWT_SECRET;
 const TOKEN_EXPIRATION = process.env.JWT_EXPIRES_IN;
 
 const checkJwt = (req, res, next) => {
-  const token = req.headers.authorization;
+  const token = req.headers.authorization && req.headers.authorization.split(' ')[1];
 
   if (!token) {
     return res.status(401).json({ message: "No token provided." });
