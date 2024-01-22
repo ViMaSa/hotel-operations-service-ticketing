17b2914c4602c2e790141bd9cb789f87032b8ecc
Victor Sarmiento <sarmiv07@gmail.com>
2024-01-21 22:38:15 -0800

    updated vue.config.js

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,10 +1,15 @@
-const { defineConfig } = require("@vue/cli-service")
+const { defineConfig } = require("@vue/cli-service");
+
+const apiTarget = process.env.NODE_ENV === 'production'
+  ? 'https://host-app-server-7f2f9249185c.herokuapp.com/'
+  : 'http://localhost:3000';
+
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: apiTarget,
         changeOrigin: true,
         pathRewrite: {
           '^/api': ''
