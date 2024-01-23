d58e48a7b973ef5c878bc58b504774dfab5bb6bc
Victor Sarmiento <sarmiv07@gmail.com>
2024-01-22 18:20:10 -0800

    airbrake testing

--- backend/main.js
+++ backend/main.js
@@ -46,7 +46,7 @@ app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/", allRoutes);
-// app.use(errorHandler);
+app.use(errorHandler);
 app.use(airbrakeExpress.makeErrorHandler(airbrake));
 
 if(process.env.NODE_ENV !== "test") {
