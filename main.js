93b1308eefbbce4c6c2d6a5f4f9706d60402ca53
Victor Sarmiento <sarmiv07@gmail.com>
2024-01-22 17:57:24 -0800

    Added airbrake

--- main.js
+++ main.js
@@ -6,6 +6,13 @@ const morgan = require("morgan");
 const { Sequelize } = require("sequelize");
 const allRoutes = require("./routes/index");
 const errorHandler = require("./middleware/errorHandler");
+const Airbrake = require('@airbrake/node');
+const airbrakeExpress = require('@airbrake/node/dist/instrumentation/express');
+
+const airbrake = new Airbrake.Notifier({
+  projectId: 544306,
+  projectKey: '4169bb2614497f7e43aaed6aa54c3233',
+});
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -37,8 +44,10 @@ app.use(cors());
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(airbrakeExpress.makeMiddleware(airbrake));
 app.use("/", allRoutes);
 app.use(errorHandler);
+app.use(airbrakeExpress.makeErrorHandler(airbrake));
 
 if(process.env.NODE_ENV !== "test") {
   app.use(morgan("combined"));
