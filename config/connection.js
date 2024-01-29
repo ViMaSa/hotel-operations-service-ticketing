4aae289dc632214b8d189e9f0631d3e996ffb28f
Victor Sarmiento <sarmiv07@gmail.com>
2024-01-28 21:29:28 -0800

    Cleaned up console.logs

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,13 @@
 const Sequelize = require('sequelize');
 const config = require('./config.js');
 
-// Select the correct configuration based on the current environment
 const env = process.env.NODE_ENV || 'development';
 const currentConfig = config[env];
 
 let sequelize;
 if (currentConfig.use_env_variable) {
-    // For production: use the DATABASE_URL environment variable
     sequelize = new Sequelize(process.env[currentConfig.use_env_variable], currentConfig);
 } else {
-    // For development and test: use individual parameters
     sequelize = new Sequelize(
         currentConfig.database,
         currentConfig.username,
