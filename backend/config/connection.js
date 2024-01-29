28bef24596008c7af5675f9ae84267063d848e07
Victor Sarmiento <sarmiv07@gmail.com>
2024-01-28 21:23:15 -0800

    updated connection.js

--- backend/config/connection.js
+++ backend/config/connection.js
@@ -1,12 +1,22 @@
-const Sequelize = require("sequelize");
-const env = process.env.NODE_ENV || "development";
-const config = require("./config")[env];
+const Sequelize = require('sequelize');
+const config = require('./config.js');
 
-const sequelize = new Sequelize(
-  config.database,
-  config.username,
-  config.password,
-  config,
-);
+// Select the correct configuration based on the current environment
+const env = process.env.NODE_ENV || 'development';
+const currentConfig = config[env];
+
+let sequelize;
+if (currentConfig.use_env_variable) {
+    // For production: use the DATABASE_URL environment variable
+    sequelize = new Sequelize(process.env[currentConfig.use_env_variable], currentConfig);
+} else {
+    // For development and test: use individual parameters
+    sequelize = new Sequelize(
+        currentConfig.database,
+        currentConfig.username,
+        currentConfig.password,
+        currentConfig
+    );
+}
 
 module.exports = sequelize;
